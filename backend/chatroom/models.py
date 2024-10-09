2d9d01e1c71dce240babccd39cac6ce3fed593b8
Ronak Vir <ronakpvir@gmail.com>
2024-10-08 23:19:40 -0700

    chatroom user count mig

--- backend/chatroom/models.py
+++ backend/chatroom/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class ChatRoom(models.Model):
     room_name = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
+    user_count = models.IntegerField(default=0)
 
     def __str__(self):
         return self.room_name
