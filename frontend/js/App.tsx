b2c0e1eb0decf705679616a3e401fa0f4dbeb4e6
Ronak Vir <ronakpvir@gmail.com>
2024-10-09 18:03:22 -0700

    luv

--- frontend/js/App.tsx
+++ frontend/js/App.tsx
@@ -1,10 +1,13 @@
 import * as Sentry from "@sentry/react";
 import cookie from "cookie";
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
+import love from "../assets/images/mclove.png";
+
 import { OpenAPI } from "./api";
-import Home from "./pages/Home";
 import Chat from "./pages/Chat";
+import Home from "./pages/Home";
 
 OpenAPI.interceptors.request.use((request) => {
   const { csrftoken } = cookie.parse(document.cookie);
@@ -17,12 +20,35 @@ OpenAPI.interceptors.request.use((request) => {
 const App = () => (
   <Sentry.ErrorBoundary fallback={<p>An error has occurred</p>}>
     <Router>
-      <Routes>
-        {/* Define routes for each page */}
-        <Route element={<Home />} path="/" /> {/* Home page */}
-        <Route element={<Chat />} path="/chat" /> {/* Chat page */}
-        {/* You can add more routes here as needed */}
-      </Routes>
+      <div
+        style={{ minHeight: "100vh", display: "flex", flexDirection: "column" }}
+      >
+        <div style={{ flex: 1 }}>
+          <Routes>
+            <Route element={<Home />} path="/" /> {/* Home page */}
+            <Route element={<Chat />} path="/chat" /> {/* Chat page */}
+          </Routes>
+        </div>
+        <footer
+          style={{
+            textAlign: "center",
+            padding: "10px",
+            position: "fixed",
+            bottom: 0,
+            left: 0,
+            right: 0,
+            width: "100%",
+          }}
+        >
+          <span>Made with </span>
+          <img
+            alt="Love"
+            src={love}
+            style={{ width: "20px", verticalAlign: "middle" }}
+          />
+          <span> by rv</span>
+        </footer>
+      </div>
     </Router>
   </Sentry.ErrorBoundary>
 );
